fea7dde721c68bb335aab3fcdb3d9afb6d7ad3b0
Neil <neilkosh001@gmail.com>
2018-09-27 18:31:51 -0400

    completed homework

---
+++ database/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Ham Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Pizza Burger', FALSE);
